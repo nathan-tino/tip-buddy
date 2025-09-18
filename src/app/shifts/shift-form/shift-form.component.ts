13546bf6fb06acf119a5dac5effcf8d60c6340cf
Nathan Tino <tinonw01@gmail.com>
2025-09-18 16:03:38 -0700

    refactor: simplify input property declarations in ShiftFormComponent. remove unnecessary import of dateservice

--- src/app/shifts/shift-form/shift-form.component.ts
+++ src/app/shifts/shift-form/shift-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ShiftFormModel } from './shift-form.model';
-import { DateService } from '../../services/date.service';
 
 // PrimeNG imports
 import { DialogModule } from 'primeng/dialog';
@@ -29,20 +28,18 @@ import { FloatLabelModule } from 'primeng/floatlabel';
     styleUrl: './shift-form.component.css'
 })
 export class ShiftFormComponent {
-    @Input() dateInput?: Date | undefined = undefined;
-    @Input() timeInput?: Date | undefined = undefined;
-    @Input() creditTipsInput: number | undefined = undefined;
-    @Input() cashTipsInput: number | undefined = undefined;
-    @Input() tipoutInput: number | undefined = undefined;
-    @Input() hoursWorkedInput: number | undefined = undefined;
+    @Input() dateInput?: Date;
+    @Input() timeInput?: Date;
+    @Input() creditTipsInput?: number;
+    @Input() cashTipsInput?: number;
+    @Input() tipoutInput?: number;
+    @Input() hoursWorkedInput?: number;
     @Input() title: string = '';
 
     @Output() submitted = new EventEmitter<ShiftFormModel>();
     @Output() cancel = new EventEmitter<void>();
     
     visible = true; // Dialog visibility
-
-    constructor(private dateService: DateService) { }
 
     onSubmit() {
         // Type guard to ensure dateInput exists before emitting
